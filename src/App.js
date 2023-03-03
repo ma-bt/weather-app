34810e9e0b4e668daeeac4bcbc5d64869cd5fce7
ma-bt <mandeerab20@gamil.com>
2023-03-03 22:56:37 +0545

    ui completed

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeandLocation from './components/TimeandLocation';
 import TemperatureandDetails from './components/TemperatureandDetails';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className="mx-auto max-w-screen-md my-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       
      <TopButtons/>
      <Inputs/>
 
      <TimeandLocation />
      <TemperatureandDetails />
+     <Forecast title="hourly forecast" />
+     <Forecast title="hourly forecast" />
      
     </div>
   );
